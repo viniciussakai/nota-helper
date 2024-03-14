2223b9614084fcf0cd84b63bf55cade29f0c1a9b
Vinicius Sakai <vinicius.sakai@tuta.io>
2024-03-14 01:21:49 -0300

    feat: create nota-helper

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,147 @@
-import Image from "next/image";
+"use client";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { useState } from "react";
+
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+const FormSchema = z.object({
+  value: z.string().min(2),
+  payment: z.string().min(2).nullable(),
+  beneficiary: z.object({
+    name: z.string().min(3),
+    birthday: z.string(),
+  }),
+  services: z.array(z.string()),
+});
+
+type FormValues = z.infer<typeof FormSchema>;
+
+const PaymentTypes = [
+  { value: "debit", label: "Débito" },
+  { value: "credit1", label: "Crédito 1x" },
+  { value: "credit2", label: "Crédito 2x" },
+  { value: "credit3", label: "Crédito 3x" },
+  { value: "credit4", label: "Crédito 4x" },
+  { value: "credit5", label: "Crédito 5x" },
+];
+
+const ServiceTypes = [
+  { value: "service1", label: "TOMOGRAFIA" },
+  { value: "service2", label: "RADIOGRAFIA PERIAPICAL" },
+  { value: "service3", label: "DOCUMENTAÇAO ODONTOLOGICA" },
+  { value: "service4", label: "Serviço 4" },
+  { value: "service5", label: "Serviço 5" },
+];
 
 export default function Home() {
+  const { register, handleSubmit, reset } = useForm({});
+
+  const [result, setResult] = useState("");
+
+  const onSubmit = (data: any) => {
+    let result = "";
+    const tribute = (Number(data.value) * 0.1631).toLocaleString("pt-BR", {
+      minimumFractionDigits: 2,
+      maximumFractionDigits: 2,
+    });
+
+    for (const ds of data.services) {
+      const serviceName = ServiceTypes.find(
+        (service) => service.value == ds
+      )?.label;
+
+      result += `${serviceName}\n`;
+    }
+
+    const payment = PaymentTypes.find(
+      (payment) => payment.value === data.payment
+    )?.label;
+
+    result += `Valor Aproximado de Tributos Lei 12.741 - 16,51% -  R$${tribute}\n`;
+    result += `Forma de Pagamento: ${payment}\n`;
+
+    if (data.beneficiary.name) {
+      result += `Beneficiário: ${data.beneficiary.name}\n`;
+      result += `DN: ${data.beneficiary.birthday}\n`;
+    }
+
+    setResult(result);
+    reset({
+      services: data.services,
+      payment: data.payment,
+      value: data.value,
+      beneficiary: {
+        name: "",
+        birthday: "",
+      },
+    });
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="flex flex-1 min-h-screen   items-center justify-center gap-8 px-10">
+      <form
+        className="flex flex-col gap-7 w-full  max-w-smmt-20"
+        onSubmit={handleSubmit(onSubmit)}
+      >
+        <h1 className="text-2xl font-bold text-center">Formulário</h1>
+
+        <h2 className="text-xl font-bold">Serviços</h2>
+
+        <div className="flex flex-col gap-2">
+          {ServiceTypes.map((service, i) => (
+            <div className="items-top flex space-x-2">
+              <input
+                type="checkbox"
+                value={service.value}
+                {...register("services")}
+              />
+              <div className="grid gap-1.5 leading-none">
+                <label className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70">
+                  {service.label}
+                </label>
+              </div>
+            </div>
+          ))}
         </div>
-      </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+        <Input type="text" placeholder="Valor" {...register("value")} />
+
+        <select
+          {...register("payment")}
+          className="text-sm ring-offset-black py-2 px-3 bg-background border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-primary-500 focus:border-transparent"
+        >
+          <option value="">Forma de pagamento</option>
+          {PaymentTypes.map((payment) => (
+            <option key={payment.value} value={payment.value}>
+              {payment.label}
+            </option>
+          ))}
+        </select>
+
+        <Input
+          type="text"
+          placeholder="Beneficiário"
+          {...register("beneficiary.name")}
         />
-      </div>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+        <Input
+          type="text"
+          placeholder="Data de Nascimento do Beneficiário"
+          {...register("beneficiary.birthday")}
+        />
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+        <Button type="submit">Enviar</Button>
+      </form>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      {result && (
+        <div className="flex flex-col gap-2  max-w-sm">
+          <h2 className="text-xl font-bold">Resultado</h2>
+          <pre>{result}</pre>
+          <Button onClick={() => setResult("")}>Limpar</Button>
+        </div>
+      )}
     </main>
   );
 }
